7fd798196ab044e939c23d0ffbd8319df4a63f92
John Poston <john.poston2@outlook.com>
2022-11-17 20:53:40 -0500

    added ProductFile.cs

---
+++ MidtermProject/MidtermProject/ProductFile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidtermProject
+{
+    public class ProductFile
+    {
+        public List<Product> products { get; set; }
+        public ProductFile()
+        {
+            products = new List<Product>();
+            products.AddRange(GetAllProducts());
+        }
+        List<Product> GetAllProducts()
+        {
+            //string fileName = "Product.txt";
+            string fileName = @"\.\MidtermProject\Product.txt";
+            string line = "";
+            string[] values;
+            StreamReader reader = new StreamReader(fileName);
+
+            while (true)
+            {
+                line = reader.ReadLine();
+                if (line == null) { break; }
+                values = line.Split('|');
+                items.Add(new Product() { Name = values[0], Description = values[1], Price = values[2] });
+            }
+
+            reader.Close();
+            return items;
+        }
+    }
+}
